6063956c016bdd1b20c00be6a45b1201054074f5
Florin D <fredtux@users.noreply.github.com>
2021-11-24 19:03:16 +0200

    Single out key from output

--- crack.py
+++ crack.py
@@ -3,7 +3,6 @@ from argparse import ArgumentParser
 
 def get_arguments():
     parser = ArgumentParser(description="Encrypts a file using xor")
-    #parser.add_argument("key", metavar="key", type=str, help="XOR key")
     parser.add_argument("input", metavar="input_file", type=str, help="File that will be encrypted")
     parser.add_argument("output", metavar="output", type=str, help="File containing the encrypted text")
 
@@ -21,12 +20,36 @@ def get_input_bytearray(input_file):
 
 def crack(binput, boutput):
     i = 0
-    k =[]
+    k = []
     for char in binput:
         k.append(chr(binput[i] ^ boutput[i]))
         i += 1
 
     return k
+
+
+def encrypt(key, binput):
+    res = []
+
+    i = 0
+    kl = len(key)
+    for _, value in enumerate(binput):
+        res.append(value ^ key[i])
+
+        i = (i + 1) % kl  # Rotate through key
+
+    return res
+
+
+def bisEqual(b1, b2):
+    if len(b1) != len(b2):
+        return False
+
+    for i in range(len(b1)):
+        if b1[i] != b2[i]:
+            return False
+
+    return True
 
 
 # Get arguments
@@ -39,8 +62,19 @@ binput = get_input_bytearray(args.input)
 boutput = get_input_bytearray(args.output)
 
 # Crack the key
-key = crack(binput, boutput)
+rkey = crack(binput, boutput)  # Returned key
+key = ""
 
-for i in range(len(key)):
-    print(key[i], end = '')
+for i in range(10, 16):
+    pkey = "".join(rkey[:i + 1])  # Possible key
+    bkey = bytearray(pkey, encoding="UTF-8")  # Byte-array key
+    if bisEqual(encrypt(bkey, binput), boutput):
+        key = pkey
+        break
+else:
+    key = []
+    for i in range(len(rkey)):
+        key.append(rkey[i])
+    key = "".join(key)
 
+print(key)
