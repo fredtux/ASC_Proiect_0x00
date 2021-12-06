a3eca2002781ebb952bc1d1952f23488e6ed0213
Florin Dinu <fredtux@users.noreply.github.com>
2021-12-06 23:44:33 +0200

    Create break.py

---
+++ break.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+
+def most_frequent(List):
+    occurence_count = Counter(List)
+    return occurence_count.most_common(1)[0][0]
+
+
+with open("output", "rb") as f:
+    binput = bytearray(f.read())
+    for k in range(10, 16):
+        key = []
+        for j in range(k):
+            highest = []
+            for i in range(j,len(binput), k):
+                highest.append(binput[i] ^ 0x20)
+            key.append(chr(most_frequent(highest)))
+
+        isValid = True
+
+        for i in key:
+            if not i.isalnum():
+                isValid = False
+                break
+        if isValid:
+            print("".join(key))
